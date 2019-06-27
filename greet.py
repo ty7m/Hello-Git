ed516326f26cfe7ffbfcf86d6bbe019b4fac936d
takato yamada <takatoyamada@MacBook-Pro7.local>
2019-06-27 15:26:26 +0900

    初めてのGitHub！

---
+++ greet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 13:34:39 2019
+
+@author: takatoyamada
+"""
+
+def greet():
+    """<挨拶の仕方>
+    おはようございます
+    """
+    print("Hello world")
+greet()
+help(greet)
